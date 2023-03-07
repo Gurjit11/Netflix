18c7181aa42f20bc367cfa1c0f3e7b44990990d0
Gurjit Singh <102173730+Gurjit11@users.noreply.github.com>
2023-03-07 11:58:11 +0530

    Update requests.js

--- src/requests.js
+++ src/requests.js
@@ -1,8 +1,6 @@
-const api_key = '965ad452a22ad622cdce2767a9dbc4ee'
 export const requests = {
-    latest : `https://api.themoviedb.org/3/movie/latest?api_key=${api_key}&language=en-US`,
-    popular: `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`,
-    top_rated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${api_key}&language=en-US&page=1`,
-    upcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${api_key}&language=en-US&page=1`,
-
+    latest : `https://api.themoviedb.org/3/movie/latest?api_key=${process.env.REACT_APP_API_KEY_MOVIES}&language=en-US`,
+    popular: `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY_MOVIES}&language=en-US&page=1`,
+    top_rated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.REACT_APP_API_KEY_MOVIES}&language=en-US&page=1`,
+    upcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_API_KEY_MOVIES}&language=en-US&page=1`,
 }
